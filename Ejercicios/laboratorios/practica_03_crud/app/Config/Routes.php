ddb2d3bccd521b61e431468c91b8f6f007af4d36
oliber246 <sonccomachacajordan@gmail.com>
2025-12-18 22:07:16 -0500

    Agregando pestaña Backend y practicas 3

--- Ejercicios/laboratorios/practica_03_crud/app/Config/Routes.php
+++ Ejercicios/laboratorios/practica_03_crud/app/Config/Routes.php
@@ -5,7 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
 $routes->get('/', 'Crud::index');
 $routes->post('/crear', 'Crud::crear');
 $routes->get('/eliminar/(:num)', 'Crud::eliminar/$1');
